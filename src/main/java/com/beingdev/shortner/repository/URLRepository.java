5f5c8762f9762a4a8b13f3a3d64afad78f015d4a
Kshitij <singh_kshitij@yahoo.com>
2019-01-19 18:03:50 +0530

    Update URLRepository.java

--- src/main/java/com/beingdev/shortner/repository/URLRepository.java
+++ src/main/java/com/beingdev/shortner/repository/URLRepository.java
@@ -12,39 +12,40 @@ import redis.clients.jedis.Jedis;
 
 @Repository
 public class URLRepository {
-    private final Jedis jedis;
+	
     private final String idKey;
     private final String urlKey;
     private static final Logger LOGGER = LoggerFactory.getLogger(URLRepository.class);
 
-    public URLRepository() throws URISyntaxException {
-        
-    	this.jedis = new SpringConfig().getJedisPool().getResource();
+    public URLRepository() {
     	
         this.idKey = "id";
         this.urlKey = "url:";
     }
+    
+    private Jedis getJedisInstance() {
+    	return new SpringConfig().getJedisPool().getResource();
+    }
 
     public URLRepository(Jedis jedis, String idKey, String urlKey) {
-        this.jedis = jedis;
         this.idKey = idKey;
         this.urlKey = urlKey;
     }
 
     public Long incrementID() {
-        Long id = jedis.incr(idKey);
+        Long id = getJedisInstance().incr(idKey);
         LOGGER.info("Incrementing ID: {}", id-1);
         return id - 1;
     }
 
     public void saveUrl(String key, String longUrl) {
         LOGGER.info("Saving: {} at {}", longUrl, key);
-        jedis.hset(urlKey, key, longUrl);
+        getJedisInstance().hset(urlKey, key, longUrl);
     }
 
     public String getUrl(Long id) throws Exception {
         LOGGER.info("Retrieving at {}", id);
-        String url = jedis.hget(urlKey, "url:"+id);
+        String url = getJedisInstance().hget(urlKey, "url:"+id);
         LOGGER.info("Retrieved {} at {}", url ,id);
         if (url == null) {
             throw new Exception("URL at key" + id + " does not exist");
