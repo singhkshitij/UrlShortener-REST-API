d9ee6c4a0276b298187cca042494df749043663d
Kshitij <singh_kshitij@yahoo.com>
2019-01-20 19:11:10 +0530

    Swagger UI

--- src/main/java/com/beingdev/shortner/controller/URLController.java
+++ src/main/java/com/beingdev/shortner/controller/URLController.java
@@ -1,19 +1,25 @@
 package com.beingdev.shortner.controller;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
 import com.beingdev.shortner.service.URLConverterService;
 import com.beingdev.shortner.utils.URLValidator;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.Valid;
-import java.io.IOException;
-import java.net.URISyntaxException;
 
 
 @RestController
