19c92cfe8ac7882150e001a7a6405f20e11d6123
Kshitij <singh_kshitij@yahoo.com>
2019-01-20 17:10:36 +0530

    Update SpringConfig.java

--- src/main/java/com/beingdev/shortner/config/SpringConfig.java
+++ src/main/java/com/beingdev/shortner/config/SpringConfig.java
@@ -8,20 +8,15 @@ import org.springframework.context.annotation.Configuration;
 
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
-import redis.clients.jedis.Protocol;
 
 @Configuration
 public class SpringConfig {
 
     @Bean
-    public JedisPool getJedisPool() {
+    public static JedisPool getJedisPool() {
         try {
-            URI redisURI = new URI(System.getenv("REDISTOGO_URL"));
-            return new JedisPool(new JedisPoolConfig(),
-                    redisURI.getHost(),
-                    redisURI.getPort(),
-                    Protocol.DEFAULT_TIMEOUT,
-                    redisURI.getUserInfo().split(":",2)[1]);
+            URI redisURI = new URI(System.getenv("REDIS_URL"));
+            return new JedisPool(new JedisPoolConfig(), redisURI);
         } catch (URISyntaxException e) {
             throw new RuntimeException("Redis couldn't be configured from URL in REDISTOGO_URL env var:"+
                                         System.getenv("REDISTOGO_URL"));
