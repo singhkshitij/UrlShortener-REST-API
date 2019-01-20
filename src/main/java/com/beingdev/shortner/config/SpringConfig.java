a8eb2c3b9d30c7a45cfc080ba381fb508fc12931
Kshitij <singh_kshitij@yahoo.com>
2019-01-20 17:16:43 +0530

    Update SpringConfig.java

--- src/main/java/com/beingdev/shortner/config/SpringConfig.java
+++ src/main/java/com/beingdev/shortner/config/SpringConfig.java
@@ -16,7 +16,15 @@ public class SpringConfig {
     public static JedisPool getJedisPool() {
         try {
             URI redisURI = new URI(System.getenv("REDIS_URL"));
-            return new JedisPool(new JedisPoolConfig(), redisURI);
+            
+            JedisPoolConfig poolConfig = new JedisPoolConfig();
+            poolConfig.setMaxTotal(100);
+            poolConfig.setMaxIdle(20);
+            poolConfig.setTestOnBorrow(true);
+            poolConfig.setTestOnReturn(true);
+            poolConfig.setTestWhileIdle(true);
+ 
+            return new JedisPool( poolConfig, redisURI);
         } catch (URISyntaxException e) {
             throw new RuntimeException("Redis couldn't be configured from URL in REDISTOGO_URL env var:"+
                                         System.getenv("REDISTOGO_URL"));
