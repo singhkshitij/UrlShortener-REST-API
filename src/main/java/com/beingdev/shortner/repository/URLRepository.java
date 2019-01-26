972ca18b2be0e42e58c3753eea67581e7111440b
Kshitij <singh_kshitij@yahoo.com>
2019-01-26 10:12:47 +0530

    Swagger Ui

--- src/main/java/com/beingdev/shortner/repository/URLRepository.java
+++ src/main/java/com/beingdev/shortner/repository/URLRepository.java
@@ -1,7 +1,5 @@
 package com.beingdev.shortner.repository;
 
-import java.net.URISyntaxException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
