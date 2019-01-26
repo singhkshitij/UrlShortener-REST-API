d141847417fe6920f6ec26df282c56cb0849446a
Kshitij <singh_kshitij@yahoo.com>
2019-01-26 17:25:56 +0530

    Update URLRepository.java

--- src/main/java/com/beingdev/shortner/repository/URLRepository.java
+++ src/main/java/com/beingdev/shortner/repository/URLRepository.java
@@ -51,7 +51,7 @@ public class URLRepository {
 
 	public boolean isCustomUrlAvailable(Long customUrlId) {
 		LOGGER.info("Inside isCustomURLAvailable");
-		if (reJedis.hget(urlKey, "url:" + customUrlId) != null) {
+		if (reJedis.hget(urlKey, "url:" + customUrlId) == null) {
 			LOGGER.info("Inside if of isCustomURLAvailable");
 			return true;
 		}
